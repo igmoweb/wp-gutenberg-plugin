5703c684660e0d18444fa22f507826503a9977d9
Ignacio Cruz <igmoweb@gmail.com>
2020-01-17 11:27:42 +0100

    No need to commit dotfiles

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [ '@wordpress/default' ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    [ 'transform-react-jsx', {
-      pragma: 'wp.element.createElement',
-    } ],
-  ],
-};
